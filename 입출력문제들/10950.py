fbc4320b16318de91eb3fc3df48da2163971288b
wakeupmakeup <100953166+wakeupmakeup@users.noreply.github.com>
2022-05-10 23:47:06 +0900

    파이썬 입출력문제

---
+++ 입출력문제들/10950.py
@@ -0,0 +1,7 @@
+a = 0
+b = 0 
+T = int(input())
+
+for i in range(T):
+    a, b = input().split()
+    print(int(a)+int(b))
---
+++ 입출력문제들/10952.py
@@ -0,0 +1,8 @@
+import sys
+
+for i in sys.stdin:
+    a, b = map(int, i.split())
+    if(a == 0 and b == 0):
+        break
+    else:
+        print(a+b)
