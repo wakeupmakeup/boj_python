1ccd46974fb0065141ccb8e7e17d8ccbc42a3445
wakeupmakeup <100953166+wakeupmakeup@users.noreply.github.com>
2025-06-16 16:10:15 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,12 +1,17 @@
 h, m = map(int, input().split())
 
-if m - 45 < 0:
-    m = m + 60 - 45
-    if h == 0:
-        h = 23
-    else:
-        h -= 1
-else:
-    m -= 45
+if (m - 45) < 0:
 
-print(h, m)
+    if ((h - 1) < 0):
+        h = (h - 1) + 24
+        m = (m - 45) + 60
+        print(h,m)
+
+    elif ((h - 1) >= 0):
+        h = (h - 1)
+        m = (m - 45) + 60
+        print(h, m)
+
+else:
+    m = m-45
+    print(h, m)
